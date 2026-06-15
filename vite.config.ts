b2c4d32e82e5a4627b36f9ac3b20693129be6adf
Nick <31907977+zt64@users.noreply.github.com>
2026-06-15 14:49:11 +0000

    add image optimization plugin and remove unused plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from "vite";
-import devtoolsJson from "vite-plugin-devtools-json";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
     plugins: [
-        devtoolsJson(),
-        tailwindcss(),
+        ViteImageOptimizer(),
+        tailwindcss()
     ],
 });
